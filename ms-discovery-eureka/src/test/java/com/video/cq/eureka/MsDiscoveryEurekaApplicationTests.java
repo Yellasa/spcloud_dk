da04df52890eeb0972698c34c2b65a1210a3ea4e
Yellasa <860006242@qq.com>
2018-01-30 10:28:30 +0800

    init eureka

---
+++ ms-discovery-eureka/src/test/java/com/video/cq/eureka/MsDiscoveryEurekaApplicationTests.java
@@ -0,0 +1,16 @@
+package com.video.cq.eureka;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.junit4.SpringRunner;
+
+@RunWith(SpringRunner.class)
+@SpringBootTest
+public class MsDiscoveryEurekaApplicationTests {
+
+	@Test
+	public void contextLoads() {
+	}
+
+}
