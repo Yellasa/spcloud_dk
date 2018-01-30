c534047a20f553c8d7e58c8337d50af347f28ad8
Yellasa <860006242@qq.com>
2018-01-30 16:27:22 +0800

    单独认证模块

--- ms-discovery-eureka-authen/src/main/java/com/video/cq/eureka/MsDiscoveryEurekaAuthenApplication.java
+++ ms-discovery-eureka-authen/src/main/java/com/video/cq/eureka/MsDiscoveryEurekaAuthenApplication.java
@@ -6,10 +6,10 @@ import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
 @SpringBootApplication
 @EnableEurekaServer
-public class MsDiscoveryEurekaApplication
+public class MsDiscoveryEurekaAuthenApplication
 {
 
 	public static void main(String[] args) {
-		SpringApplication.run(MsDiscoveryEurekaApplication.class, args);
+		SpringApplication.run(MsDiscoveryEurekaAuthenApplication.class, args);
 	}
 }
