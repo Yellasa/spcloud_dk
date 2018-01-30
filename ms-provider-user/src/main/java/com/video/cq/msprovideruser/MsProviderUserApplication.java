aec08759ab61ddbfa680156e6ca639f5efc54a7e
Yellasa <860006242@qq.com>
2018-01-30 16:26:01 +0800

    添加security认证

--- ms-provider-user/src/main/java/com/video/cq/msprovideruser/MsProviderUserApplication.java
+++ ms-provider-user/src/main/java/com/video/cq/msprovideruser/MsProviderUserApplication.java
@@ -2,8 +2,10 @@ package com.video.cq.msprovideruser;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
+@EnableDiscoveryClient
 public class MsProviderUserApplication {
 
 	public static void main(String[] args) {
